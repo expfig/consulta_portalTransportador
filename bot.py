52e88a34ad2ab922f20c1c11d1f61472859b28d0
JoaoAnacletoFIG <joao.meira@grupofigueiredo.com.br>
2024-01-12 14:57:39 -0300

    first commit

---
+++ bot.py
@@ -0,0 +1,98 @@
+from botcity.maestro import *
+from main import PortalTransportador
+from datetime import datetime, timedelta
+
+BotMaestroSDK.RAISE_NOT_CONNECTED = False
+
+
+
+def main():
+    maestro = BotMaestroSDK.from_sys_args()
+    execution = maestro.get_execution()
+    portal_password = maestro.get_credential(label="consulta_PortalTransportador", key="PORTAL_PASSWORD")
+    db_password = maestro.get_credential(label="consulta_PortalTransportador", key="DB_PASSWORD")
+    
+    try:
+        bot = PortalTransportador(execution.parameters, portal_password, db_password)
+        bot.connect_to_postgres()
+        bot.get_cte_tg99()
+        date = (datetime.now() - timedelta(days=3))
+        date = date.strftime("%d/%m/%Y")
+        data = maestro.get_log(activity_label="consulta_PortalTransportador", date=date)
+        
+        for cte_num in bot.cte_nums:
+            for item in data:
+                if item['nconh'] == cte_num[0] and item['msg'] != "Não encontrado na base Vega":
+                    bot.cte_nums.remove(cte_num)
+                    break  
+        
+        if len(bot.cte_nums) > 0:
+            bot.initialize_driver()
+            bot.login()
+            bot.navigate_to_portal()
+            bot.select_company()
+            bot.navigate_to_custo_frete()
+            bot.search_ctes()
+            bot.get_table_result()
+            bot.format_data_table()
+            bot.verify_value_ctes()
+            
+            for data in bot.data:
+                alerta = 'Não'
+                if data[5] == "Diferença de valor encontrada":
+                    maestro.alert(
+                        task_id=execution.task_id,
+                        title="Frete com valores divergentes encontrado!",
+                        message= f"N conhecimento: {data[1]} - Placa: {data[2]} - Data NFe: {data[4]} - Valor da Usina: {data[6]} - Valor do cte: {data[7]} - Diferença: {data[8]}",
+                        alert_type=AlertType.INFO
+                    )
+                    alerta = 'Sim'
+                    
+                maestro.new_log_entry(
+                    activity_label="consulta_PortalTransportador",
+                    values = {
+                        "nconh": data[1],
+                        "placa": data[2],
+                        "motorista": data[3],
+                        "dtnotafisc": data[4],
+                        "msg": data[5],
+                        "usina_value": data[6],
+                        "cte_value": data[7],
+                        "diff": data[8],
+                        "alerta": alerta
+                    }
+                )
+                
+        maestro.finish_task(
+            task_id=execution.task_id,
+            status=AutomationTaskFinishStatus.SUCCESS,
+            message="Tarefa foi concluída com sucesso."
+        ) 
+        
+    except Exception as erro:
+        print(f"Erro inesperado: {erro}")
+        try:
+            bot.driver.save_screenshot('erro.png')
+            maestro.error(
+                task_id=execution.task_id,
+                exception=erro,
+                screenshot='erro.png'
+            )
+        except:
+            maestro.error(
+                task_id=execution.task_id,
+                exception=erro
+            )
+            
+        maestro.finish_task(
+            task_id=execution.task_id,
+            status=AutomationTaskFinishStatus.FAILED,
+            message="Tarefa falhou."
+        )
+        
+    finally:
+        bot.close_driver()
+
+
+if __name__ == '__main__':
+    main()
